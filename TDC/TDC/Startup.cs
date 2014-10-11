4a92f3acc576d15845507a36899faa6a2fd41e34
sepehr500 <sepehr411@gmail.com>
2014-10-11 19:56:06 -0400

    First Commit!

---
+++ TDC/TDC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TDC.Startup))]
+namespace TDC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
