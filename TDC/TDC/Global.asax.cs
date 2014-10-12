da855d95fae7756944045374cc88de78c60ba16d
Chris Zimmerman <chrisxzimmerman@gmail.com>
2014-10-11 20:29:45 -0400

    I am commiting a comment in Global.asax.cs to be removed later.

--- TDC/TDC/Global.asax.cs
+++ TDC/TDC/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+//Test Comment!!!!
 namespace TDC
 {
     public class MvcApplication : System.Web.HttpApplication
