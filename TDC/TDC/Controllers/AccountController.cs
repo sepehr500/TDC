7331036b6164f164886cffae9da4f8b1831a33ac
Chris Zimmerman <chrisxzimmerman@gmail.com>
2014-12-07 13:00:17 -0500

    Main Screen UI Update
    Removed Menu Items "Contact" and "About" from global markup page. Also
    inserted "Two Dollar Challenge" into several obvious places on the main
    screen.

--- TDC/TDC/Controllers/AccountController.cs
+++ TDC/TDC/Controllers/AccountController.cs
@@ -112,7 +112,7 @@ namespace TDC.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Test", "Home");
                 }
                 else
                 {
