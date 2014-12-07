1e00e70d6adb5b0f6de3521a2300e44680bc0d5a
sepehr500 <sepehr411@gmail.com>
2014-12-07 13:08:16 -0500

    Added getUser method and get balance method

--- TDC/TDC/Controllers/HomeController.cs
+++ TDC/TDC/Controllers/HomeController.cs
@@ -6,15 +6,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TDC.Models;
+using TDC.Tools;
 
 namespace TDC.Controllers
 {
     public class HomeController : Controller
     {
         //This shows how to get currently signed in user.
-        protected ApplicationDbContext ApplicationDbContext { get; set; }
+        
 
-        protected UserManager<User> UserManager { get; set; }
+        
 
         public ActionResult Index()
         {
@@ -24,10 +25,10 @@ namespace TDC.Controllers
 
         public ActionResult About()
         {
-            this.ApplicationDbContext = new ApplicationDbContext();
-            this.UserManager = new UserManager<User>(new UserStore<User>(this.ApplicationDbContext));
+            
             ViewBag.Message = "Your application description page.";
-            var user = UserManager.FindById(User.Identity.GetUserId());
+            //use this to get current user
+            var userTest = UserActions.getUser(User.Identity.GetUserId());
             return View();
         }
 
---
+++ TDC/TDC/Tools/UserActions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TDC.Models;
+
+namespace TDC.Tools
+{
+    public class UserActions 
+    {
+
+        //use this to get current user
+        public static User getUser(string id)
+        {
+            
+            var ApplicationDbContext = new ApplicationDbContext();
+            var UserManager = new UserManager<User>(new UserStore<User>(ApplicationDbContext));
+            var user = UserManager.FindById(id);
+            return user;
+        }
+
+        //Returns the users balance
+        public static decimal getBalance(string id) {
+            decimal total = 0;
+            
+            var ApplicationDbContext = new ApplicationDbContext();
+            var UserManager = new UserManager<User>(new UserStore<User>(ApplicationDbContext));
+            var user = UserManager.FindById(id);
+            
+            foreach (Income x in user.Income )
+            {
+                total += x.Amount;
+            }
+            foreach (Expense x in user.Expense)
+            {
+                total += x.cost;
+            }
+            foreach (ShockUser x in user.ShockUser)
+            {
+                total += x.ShockLU.Amount;
+            }
+
+            return total;
+        
+        }
+
+
+
+
+    }
+
+    
+
+
+
+
+
+}
