ca543c8cb721c227cc75f9a1b48807f983123ce5
sepehr500 <sepehr411@gmail.com>
2014-12-14 22:33:24 -0500

    IDK

---
+++ TDC/TDC/Migrations/201412150241405_another.cs
@@ -0,0 +1,16 @@
+namespace TDC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class another : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
