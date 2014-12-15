4fa17399d916404034ffa305c962b2a95dc0b406
sepehr500 <sepehr411@gmail.com>
2014-12-14 23:56:56 -0500

    Ignore this

--- TDC/TDC/Controllers/HomeController.cs
+++ TDC/TDC/Controllers/HomeController.cs
@@ -13,8 +13,8 @@ namespace TDC.Controllers
     public class HomeController : Controller
     {
         //This shows how to get currently signed in user.
-        
 
+        private ApplicationDbContext db = new ApplicationDbContext();
         
 
         public ActionResult Index()
@@ -56,5 +56,32 @@ namespace TDC.Controllers
             ViewBag.teamTotal = TeamStats.getPopItem();
             return View();
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Expenses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,product,cost,UserID")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                expense.cost = expense.cost * -1;
+                expense.UserID = User.Identity.GetUserId();
+                expense.Date = DateTime.Now;
+                db.Expenses.Add(expense);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(expense);
+        }
+
+
     }
 }
